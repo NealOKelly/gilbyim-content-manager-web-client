8f72e62bdaebc34b9910d7c1743bad68a2edc547
Neal O'Kelly <neal@recordstransformation.co.uk>
2020-05-21 19:12:27 +0100

    Comment out the last vestiges of Mlacolm's code that was causing Internet Explorer to hang.

--- Content/custom/js/role-specific-script.js
+++ Content/custom/js/role-specific-script.js
@@ -2,7 +2,7 @@ function addUserClass(){
 	$("body").addClass($("#globalSearch [data-bind='text:profileUserType']").text().replace(" ", ""));
 }
 $(document).ready(function(){
-	$("body").bind("DOMSubtreeModified", function() {
+//	$("body").bind("DOMSubtreeModified", function() {
 		addUserClass();
-	});
+//	});
 });
